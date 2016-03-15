cfdff926b6acb16660ef4c90618af165d4c35f34
Stefan Jorgensen <stefantj@stanford.edu>
2016-03-15 09:54:07 -0700

    Minor updates

--- solutions.jl
+++ solutions.jl
@@ -41,7 +41,70 @@ function solve_op(solver::GurobiExactSolver, op)
     return path
 end
 
-# Uses LP-relaxation to approximately solve OP
+function solve_exact_op(solver::GurobiExactSolver, op, b_min)
+    path = build_path(op, gurobi_solve_exact(op, b_min));
+    @assert distance(op,path) <= op.distance_limit
+    return path
+end
+
+function gurobi_solve_exact(op, bmin; output=0, initial::OPSolution=OPSolution())
+    m = Model(solver=Gurobi.GurobiSolver(OutputFlag=output))
+    N = length(op) # defined as the number of vertices in the problem.
+
+    without_start = [1:op.start-1; op.start+1:N]
+    without_stop = [1:op.stop-1; op.stop+1:N]
+    without_both = intersect(without_start, without_stop)
+
+    @defVar(m, x[1:N,1:N], Bin) #NxN array of binary variables - x[i,j] == 1 means j is visited just after i
+    @defVar(m, t[1:N]) #Nx1 array of floating point allocations
+    @defVar(m, 2 <= u[without_start] <= N, Int) # is u defined here, or elsewhere? u is degree?
+
+    # For hot-starting solver?
+    if !is(initial.x, nothing)
+        setValue(x, initial.x)
+        for keytuple in keys(u)
+            key = keytuple[1]
+            setValue(u[key], initial.u[key])
+        end
+    end
+
+# Add square-root here
+    @defNLExpr(obj, sum{ sum{op.r[i]*x[i,j]*sqrt(t[i]+bmin), j=1:N}, i=1:N})
+    # sum reward of visited nodes
+    @setNLObjective(m, Max, obj)#sum{ sum{op.r[i]*x[i,j]*sqrt(t[i] + bmin), j=1:N}, i=1:N })
+
+    @addConstraint(m, min_time[k=1:N], -t[k] <= 0) 
+    @addConstraint(m, max_time, sum{t[i]+bmin, i=1:N} <= op.distance_limit)
+
+    # limit one child per parent
+    @addConstraint(m, sum{x[op.start,j], j=without_start} == 1)
+    @addConstraint(m, sum{x[i,op.stop], i=without_stop} == 1)
+
+    # problem
+    @addConstraint(m, connectivity[k=without_both], sum{x[i,k], i=1:N} == sum{x[k,j], j=1:N})
+
+    @addConstraint(m, once[k=1:N], sum{x[k,j], j=1:N} <= 1)
+    @addConstraint(m, sum{ sum{op.distances[i,j]*x[i,j], j=1:N}, i=1:N } <= op.distance_limit)
+    @addConstraint(m, nosubtour[i=without_start,j=without_start], u[i]-u[j]+1 <= (N-1)*(1-x[i,j]))
+
+    if op.start != op.stop
+        @addConstraint(m, sum{x[op.stop,i],i=1:N} == 0)
+    end
+
+    status = solve(m)
+
+    if status != :Optimal
+        warn("Not solved to optimality:\n$op")
+    end
+
+    soln = OPSolution(getValue(x), getValue(u))
+
+    if distance(op,build_path(op,soln)) > op.distance_limit
+        warn("Path Length: $(distance(op,build_path(op,soln))), Limit: $(op.distance_limit)")
+    end
+    return soln
+end
+
 function gurobi_solve(op; output=0, initial::OPSolution=OPSolution())
     m = Model(solver=Gurobi.GurobiSolver(OutputFlag=output))
     N = length(op) # defined as the number of vertices in the problem.
--- walking_orienteers.jl
+++ walking_orienteers.jl
@@ -1,185 +1,13 @@
 # Implementation of walking orienteers algorithm. 
 
-
 include("OP_Solver.jl")
+include("utils.jl")
 include("../Julia\ FMT/Libraries/GPR/GPR.jl")
 
 using GPR
 using OP_Solver
-#using UnicodePlots
+using PyCall
 using PyPlot
-
-
-#### Helper functions ####
-
-# sum_path_dict(path, times_dict)
-#  helper function to sum over a dict without directly accessing keys 
-#  (avoids some weird overflow-like symptoms)
-function sum_path_dict(path, times_dict)
-    s = 0;
-    [s+=times_dict[p] for p in path]
-    return s
-end
-
-# generate_lattice(N)
-#  Generates N^2 + (N-1)^2 points in a [0,1]^dim square
-#  points are evenly spaced and only connected to their nearest neighbors
-function generate_lattice(N; dim=2, scaling=1.0)
-    positions = Vector{Vector{Float64}}()
-    
-    # Generate positions via two superimposed grids
-    for xpos = 1:N
-        for ypos = 1:N
-            xdelta = 0.
-            pos = [float((xpos-1)/(N-1)*scaling), float((ypos-1)/(N-1)*scaling)]
-            push!(positions, Vector{Float64}(pos))
-        end
-    end
-
-    delta = 1/float(2*(N-1));
-    for xpos = 1:N-1
-        for ypos = 1:N-1 
-            pos = [float((xpos-1)/(N-1)*scaling+delta), float((ypos-1)/(N-1)*scaling+delta)]
-            push!(positions, Vector{Float64}(pos))
-        end
-    end
-
-    # Compute distances
-    # this is a stupid way to do this but you don't have to do it often so it doesn't matter too much.
-    L = length(positions)
-    distances = 1000000*ones(L,L); #large number - should really put something better here.
-    dmin = 1/(sqrt(2)*(N-1))
-    i1 = 0
-    for p1 in positions
-        i1+=1
-        i2 = 0
-        for p2 in positions
-            i2+=1
-            # check if distance is small
-            if(norm(p1-p2) <= dmin + 1e-5)
-                distances[i1,i2] = norm(p1-p2);
-            end 
-        end
-    end
-    return positions, distances
-end
-
-# generate_Gaussian_reward(pointset)
-#  samples a Gaussian process using GPR library
-#  Can be generalized to include prior data by using
-#  an existing GPR.GaussianProcessEstimate object.
-function generate_Gaussian_reward(pointset)
-    # Generate model
-    noisevar = 0.01
-    bandwidth = 0.1
-    k_SE = GPR.SquaredExponential(bandwidth)
-    model = GPR.GaussianProcess(noisevar, k_SE)
-    estimator = GPR.GaussianProcessEstimate(model, 2)
-    r = vec(GPR.sample_n(estimator, pointset))
-    reward_vals = Dict(zip(pointset, r)) # for easy lookup
-    return r, reward_vals
-end
-
-# generate_simple_reward(pointset)
-#  generates rewards using a decaying exponential function. 
-function generate_simple_reward(pointset)
-    r = ones(length(pointset))
-    i = 0;
-    for pt in pointset
-        i+=1
-       r[i] = 1#*exp(-10*norm(pt-[0.55,0.75])^2)
-    end
-    r[53] = 10;
-
-    reward_vals = Dict(zip(pointset, r)) # for easy lookup
-    return r, reward_vals
-end
-
-#### 
-
-#### Printing functions ####
-
-function print_path(pointset::Vector{Vector{Float64}}, path::Array{Float64,1}, budget_alloc::Array{Float64,1})
-    xvec = zeros(length(path))
-    yvec = zeros(length(path))
-    i = 1
-    for pp in path
-        println(pointset[pp][1], "\t", pointset[pp][2], "\t", budget_alloc[pp])
-        xvec[i] = pointset[pp][1]
-        yvec[i] = pointset[pp][2]
-        i+=1
-    end
-end
-
-
-function print_rewards(pointset, reward_vals)
-   # print as (xyz) tuples
-   println("reward_surface=[") 
-   [println(pp[1],"\t",pp[2],"\t",reward_vals[pp]) for pp in pointset]
-   println("]")
-end
-
-# deprecated
-function print_pointset()
-    N = 20 # there are N^2 + (N-1)^2 points in the lattice
-    pointset, distances = generate_lattice(N)
-    println("Pointset is")
-    #print points for debug
-    for pp in pointset
-      println(pp[1],"\t",pp[2]);
-    end
-end
-
-# assumes UnicodePlots loaded
-function plot_path_alloc(path, budget_alloc)
-
-   x = collect(1:length(path))
-   y = zeros(length(path));
-
-   iter = 0;
-   for pp in path
-      iter+=1
-      y[iter] = budget_alloc[pp]
-   end
-#   println(scatterplot(x,y))
-end
-
-# assumes PyPlot loaded
-function plot_solution(pointset, opt_path, opt_alloc, reward_vals, N)
-    # generate reward surface contour
-    # since a contour plot, use only the sparser N^2 grid.
-    C_XY = ones(N) # x,y values the same
-    [C_XY[pos] = float(pos-1)/(N-1) for pos = 1:N]
-    C_Z = zeros(N,N)
-    [[C_Z[x,y] = reward_vals[[C_XY[x],C_XY[y]]] for x=1:N] for y=1:N]
-
-    # generate path data in plottable form
-    p_x = zeros(length(opt_path))
-    p_y = zeros(length(opt_path))
-    p_z = zeros(length(opt_path))
-    bud_step = 1/(sqrt(2)*(N-1))
-    iter= 0;
-    for pp in opt_path
-        iter+=1
-        p_x[iter] = pointset[pp][2]
-        p_y[iter] = pointset[pp][1]
-        p_z[iter] = (opt_alloc[pp]/bud_step ) #reward_vals[pointset[pp]]
-    end
-
-    #plot3D(p_x,p_y,p_z);
-    #contour3D(C_XY,C_XY,C_Z./reward_scaling)
-    fig = figure("pyplot_surfaceplot", figsize=(10,10));
-    ax = fig[:add_subplot](1,1,1, projection = "3d")
-    println(ax)
-    ax[:plot3D](p_x, p_y, p_z)#, cmap=ColorMap("gray"))
-    ax[:contour3D](C_XY, C_XY, C_Z, 10, zdir="z", offset=0, cmap = ColorMap("coolwarm"))
-    zlabel("Number of visits");
-    ax[:set_zlim]([-0.000001, maximum(p_z)+1]);
-
-end
-
-
-####
 
 # waterfilling(path, rewards, budget, min_step)
 #  Allocates remaining $budget in increments of $min_step 
@@ -192,7 +20,7 @@ function waterfilling(path, rewards, budget, min_step)
     times = Dict(zip(path, min_step*ones(path)));
     b_init = sum_path_dict(path, times)
     budget_left = budget;
-    increment = min_step/10;
+    increment = min_step/1000;
 
     # Priority queue to keep track of best next step
     PQ = Collections.PriorityQueue()
@@ -227,6 +55,119 @@ function waterfilling(path, rewards, budget, min_step)
     end
 
     return float(obj_val), times
+end
+
+function sample_reward_function(x::Vector{Float64})
+    return sample_reward_function(x[1],x[2]);
+end
+# this is much easier than trying to make a stupid matching grid...
+function sample_reward_function(xpt,ypt)
+ centers = [0.5  0.5   1 
+ 		    0.702462  0.617413    1
+		    0.276299  0.0144      1 
+ 		    0.612661  0.9574     -1
+ 		    0.855629  0.427364    1
+            0.860654  0.0238044  -1
+            0.401519  0.660306   -1
+            0.280782  0.533708    1
+            0.186563  0.396171    1
+            0.404439  0.676824   -1
+            0.700535  0.834689    1 
+            0.367357  0.143921   -1
+            0.339283  0.168406   -1 
+            0.071649  0.688787    1
+            0.902341  0.39723    -1]
+    reward_val = 0;
+    for k=1:15
+        reward_val +=centers[k,3]*exp(- ((xpt-centers[k,1])^2 + (ypt-centers[k,2])^2)/0.03)
+    end
+    return reward_val;
+end
+
+function discretization_test()
+    start_node = 1;
+    end_node = 2;
+    N=20; 
+    pointset, distances = generate_grid(N)
+    # reshape for convenience
+    x_vals = zeros(N^2+2)
+    y_vals = zeros(N^2+2)
+    iter = 0;
+    for pp in pointset
+        iter+=1;
+        x_vals[iter] = pp[1]
+        y_vals[iter] = pp[2]
+    end
+
+    R_img = zeros(100,100);
+    for xindex = 1:100
+        for yindex = 1:100
+            R_img[yindex,xindex] = sample_reward_function( (xindex-1)/100, 1 - (yindex-1)/100  )
+        end
+    end
+    #imshow(R_img, extent=[0,1,0,1])
+    
+    budget = 30;
+
+    for n = 5:20
+        #Sample points
+        decimated_points, decimated_distances = generate_lattice(n)
+
+        #Sample reward function
+        r_decimated = zeros(length(decimated_points))
+        for i=1:length(decimated_points)
+            r_decimated[i] = sample_reward_function(decimated_points[i])
+        end
+
+        # Compute path for given discretization
+
+       println("Traveling from ", decimated_points[1], " to ", decimated_points[n])
+       # Form problem instance
+       op = OP_Solver.SimpleOP(r_decimated-minimum(r_decimated)+0.001, decimated_points,1.01, 1,n, decimated_distances);
+       solver=OP_Solver.GurobiExactSolver()
+
+       # compute path
+       path = OP_Solver.solve_op(solver, op)
+
+    	p_x = zeros(length(path))
+    	p_y = p_x
+    	iter= 0;
+    	for pp in path
+            iter+=1
+            # For 3D line plot
+            p_x[iter] = decimated_points[pp][2]
+            p_y[iter] = decimated_points[pp][1]
+    	end
+
+
+        # points for plotting
+        x_dec = zeros(length(decimated_points))
+        y_dec = zeros(length(decimated_points))
+
+        iter = 0;
+        for pp in decimated_points
+            iter+=1;
+            x_dec[iter] = pp[2]; y_dec[iter]=pp[1];
+        end
+  
+        # plot results and save frame
+        fig = figure(2);
+        cla()
+        #axis([0,1,0,1])
+#        scatter(x_vals, y_vals, color="black", hold=true)
+        scatter(x_dec, y_dec, color="red")
+        imshow(R_img, extent=[0,1,0,1])
+        plot(p_x,p_y, color="black")
+        numstring = string(n);
+        if(length(numstring) == 1)
+            numstring = string("00",numstring)
+        elseif(length(numstring) == 2)
+            numstring = string("0",numstring)
+        end
+        fname = string("img", numstring, ".png")
+        println(fname)
+        savefig(fname)
+    end
 end
 
 
@@ -275,7 +216,6 @@ function test()
         used_budget = (length(path))/(sqrt(2)*(N-1))
         remaining_budget = B_MAX - used_budget;
 
-        println("Filling in remaining budget...")
         # use water-filling to use the rest of the budget
         obj_val, budget_alloc = waterfilling(path, r, remaining_budget, B_step)        
         if(obj_val == -1) # means we've hit upper limit
@@ -302,6 +242,9 @@ function test()
             max_val = obj_val
             max_path = path
             max_alloc = budget_alloc
+        else
+            println("Finished search?")
+            break;
         end
     end
 
@@ -316,3 +259,36 @@ function test()
 end
 
 
+function test_exact()
+    println("starting test")
+    # Form lattice
+    N = 10 # there are N^2 + (N-1)^2 points in the lattice
+    pointset, distances = generate_lattice(N)
+    start_node = 1; # (0,0)
+    end_node = N; # (1,0)
+
+    println("Solving a problem with ", N^2+(N-1)^2, " nodes.")
+
+    # Budget values
+    B_MAX = 10;
+
+    # Reward function
+    r, reward_vals = generate_Gaussian_reward(pointset)
+#    r, reward_vals = generate_simple_reward(pointset)
+
+    # Form problem instance
+    op = OP_Solver.SimpleOP(r-minimum(r)+0.001, pointset ,B_MAX, start_node, end_node, distances);
+    solver=OP_Solver.GurobiExactSolver()
+
+    println("Solution log:\n \t B_path \t\t L_path \t\t total \t\t opt_val")
+
+    # search over budget values
+    max_val = -1
+    max_path = [];
+    max_alloc = Dict()
+    op.distance_limit = B_MAX
+    path = OP_Solver.solve_exact_op(solver, op, 1/sqrt(N))
+    println("I guess it worked?")
+
+
+end
